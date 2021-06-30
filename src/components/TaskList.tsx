5426dcaa18191cd8ae2a84067b140098ce1d8108
Emerson Bertanha <ebertanha@gmail.com>
2021-06-30 15:00:10 -0300

    Desfio concluido

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,31 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const changedTasks = tasks.map<Task>(task => task.id == id ? {
+        ...task,
+        isComplete: !task.isComplete
+      }:task);
+    setTasks(changedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id != id);
+    setTasks(filteredTasks);
   }
 
   return (
